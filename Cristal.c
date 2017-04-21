a1ebb736d4e8bd568dccde80faccf7774ce1e708
kokee07 <gamerjorge@gmail.com>
2017-04-21 16:46:46 -0300

    pull done

--- Cristal.c
+++ Cristal.c
@@ -1,5 +1,6 @@
 #include <stdio.h> 
 #include <stdlib.h> 
+<<<<<<< Updated upstream
  
  
 int main(int argc, char const *argv[]) 
@@ -123,4 +124,30 @@ int main(int argc, char const *argv[])
  	printf("\n");
   } 
   return 0; 
+=======
+#include <string.h> 
+ 
+int* splitTotal(int n, int l){ 
+  int *matriz; 
+  int pos=1; 
+  while(pos<=l){ 
+    matriz[l-pos]= n%10;
+    n=n/10; 
+    pos++; 
+  }
+  return matriz; 
+} 
+int main(int argc, char const *argv[]) 
+{ 
+  int s,n,largo; 
+  char m[8]; 
+  while(scanf("%d %d", &s, &n), 2 ){ 
+  	sprintf(m , "%d",n);
+  	printf("%s\n", m);
+    largo=strlen(m);  
+    printf("Número recibido: %i\n",n); 
+    int *matrizP=splitTotal(n, largo); 
+    
+  }
+>>>>>>> Stashed changes
 }
