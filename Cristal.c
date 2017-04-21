f12ad76eb7490a442a292dd1bb3e7c6db3034767
MalditoInsano <seba_123-bkn@hotmail.com>
2017-04-21 16:27:49 -0300

    Imprimir terminado
    la seccion de imprimir esta terminada, falta ingresar las variables correspondientes

--- Cristal.c
+++ Cristal.c
@@ -10,34 +10,117 @@ int main(int argc, char const *argv[])
       break; 
     } 
     // separar numero 
- 
+ 	
+ 	int largo = 4;
     //imprimir 
     for (int i = 0; i < (2*s)+3; ++i) // en este for se imprime cada linea 
     {
 		if (i == 0){   /// linea superior 
 			for (int k = 0; k < largo; ++k)
-			{ 
-				printf(" ");
-				for (int x = 0; x < s; ++x)
-				{
-
+			{
+				if (1){
+					printf(" ");
+					for (int m = 0; m < s; ++m){
+						printf("-");
+					}
+					printf(" ");
 				}
-			} 
+				else{
+					printf(" ");
+					for (int m = 0; m < s; ++m){
+						printf(" ");
+					}
+					printf(" ");
+				}
+			}
+			printf("\n");
 		} 
 		else if (i > 0 && i < (s + 1)){  // lineas intermedias 1 
-
+			for (int k = 0; k < largo; ++k)
+			{
+				if (1){
+					printf("|");
+				}
+				else{
+					printf(" ");
+				}
+				for (int k = 0; k < s; ++k)
+				{
+					printf(" ");
+				}
+				if (1){
+					printf("|");
+				}
+				else{
+					printf(" ");
+				}
+			} 
+			printf("\n");
 		} 
 		else if (i == (s + 1)){  // linea central  
-
+			for (int k = 0; k < largo; ++k)
+			{
+				if (1){
+					printf(" ");
+					for (int m = 0; m < s; ++m){
+						printf("-");
+					}
+					printf(" ");
+				}
+				else{
+					printf(" ");
+					for (int m = 0; m < s; ++m){
+						printf(" ");
+					}
+					printf(" ");
+				}
+			} 
+			printf("\n");
 		} 
 		else if (i > (s + 1) && i < (2*s + 2)){  // lineas intermedias 2 
-
+			for (int k = 0; k < largo; ++k)
+			{
+				if (1){
+					printf("|");
+				}
+				else{
+					printf(" ");
+				}
+				for (int k = 0; k < s; ++k)
+				{
+					printf(" ");
+				}
+				if (1){
+					printf("|");
+				}
+				else{
+					printf(" ");
+				}
+			}
+			printf("\n");
 		} 
 		else if (i == (2*s +2)){   // linea inferior 
-
+			for (int k = 0; k < largo; ++k)
+			{
+				if (1){
+					printf(" ");
+					for (int m = 0; m < s; ++m){
+						printf("-");
+					}
+					printf(" ");
+				}
+				else{
+					printf(" ");
+					for (int m = 0; m < s; ++m){
+						printf(" ");
+					}
+					printf(" ");
+				}
+			} 
+			printf("\n");
 		} 
     } 
- 
+ 	printf("\n");
   } 
   return 0; 
 }
