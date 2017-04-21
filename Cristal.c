86ec7e43c20ddb57178a9d99778e2c3bf49a8948
MalditoInsano <seba_123-bkn@hotmail.com>
2017-04-21 16:08:53 -0300

    wiri

--- Cristal.c
+++ Cristal.c
@@ -1,27 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <math.h>
-#include "molde.h"
-int main(int argc, char const *argv[]){
-	if(argc != 1){
-		fprintf(stderr, "\x1b[31m error, ejecutar como ./prog \n \x1b[0m");
-		exit(EXIT_FAILURE);
-	}
-	int s,n,d,i,j,k;
-	while(scanf("%d%d",&s, &n), 2){
-		if(s == 0){
-			exit(EXIT_FAILURE);
-		}
-		//s tamaño numero
-		//n numero a dibujar
-		while(n != 0){
-			
-		}
-		for(i = 0;i < (2*s + 3);++i){
-			for (j = 0; j < (s+2); ++j){
-				
-			}
-		}
-	}
+#include <stdio.h> 
+#include <stdlib.h> 
+ 
+ 
+int main(int argc, char const *argv[]) 
+{ 
+  int s, n; 
+  while (scanf("%i %i\n",&s,&n)==2){ 
+    if (s == 0 && n == 0){ 
+      break; 
+    } 
+    // separar numero 
+ 
+    //imprimir 
+    for (int i = 0; i < (2*s)+3; ++i) // en este for se imprime cada linea 
+    {
+		if (i == 0){   /// linea superior 
+			for (int k = 0; k < largo; ++k)
+			{ 
+				printf(" ");
+				for (int x = 0; x < s; ++x)
+				{
+
+				}
+			} 
+		} 
+		else if (i > 0 && i < (s + 1)){  // lineas intermedias 1 
+
+		} 
+		else if (i == (s + 1)){  // linea central  
+
+		} 
+		else if (i > (s + 1) && i < (2*s + 2)){  // lineas intermedias 2 
+
+		} 
+		else if (i == (2*s +2)){   // linea inferior 
+
+		} 
+    } 
+ 
+  } 
+  return 0; 
 }
